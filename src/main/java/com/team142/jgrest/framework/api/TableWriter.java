6b9536fee20259a4ab20471940ed3a9fecfb26dc
Justin Tamblyn <just1689@gmail.com>
2017-12-11 14:10:46 +0200

    TableWriter stubbed out

--- src/main/java/com/team142/jgrest/framework/api/TableWriter.java
+++ src/main/java/com/team142/jgrest/framework/api/TableWriter.java
@@ -5,10 +5,44 @@
  */
 package com.team142.jgrest.framework.api;
 
+import com.team142.jgrest.framework.generic.Deleter;
+import com.team142.jgrest.framework.generic.Inserter;
+import com.team142.jgrest.framework.generic.Updater;
+import com.team142.jgrest.model.Condition;
+import com.team142.jgrest.model.Database;
+
 /**
  *
  * @author just1689
+ * @param <T>
  */
-public class TableWriter {
+public class TableWriter<T> {
+
+    private final Database database;
+    private final String table;
+    private final Class clazz;
+
+    public TableWriter(Database database, String table, Class clazz) {
+        this.database = database;
+        this.table = table;
+        this.clazz = clazz;
+    }
+
+    public void insert(T object) throws Exception {
+        String url = UrlBuilder.getUrl(database, table);
+        Inserter.insert(url, object);
+    }
+
+    public void update(Condition condition, T object) throws Exception {
+        String url = UrlBuilder.getUrl(database, table);
+        Updater.update(url, object);
+    }
     
+    public void delete(Condition condition) throws Exception {
+        String url = UrlBuilder.getUrl(database, table);
+        Deleter.delete(url);
+    }
+    
+    
+
 }
